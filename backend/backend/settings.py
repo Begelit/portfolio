f0c9d2ecd9970b4c64456a51e90c8aab4b351ea4
Begelit <dmilevchkz@gmail.com>
2024-09-20 19:09:16 +0500

    updated frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%ux)23-ap$5qv*io#d(x!bl574pyz7=w0^8c$=6t146ly$nqly
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
